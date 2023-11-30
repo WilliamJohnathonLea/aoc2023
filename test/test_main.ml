9ff36e6c51ca84a2237106589e7161e7fb16e394
William Lea <wjlea93@gmail.com>
2023-11-30 14:06:36 +0000

    last demo test

--- test/test_main.ml
+++ test/test_main.ml
@@ -6,9 +6,13 @@ let test_add_positives () =
 let test_add_negatives () =
   Alcotest.(check int) "should be -3" (-3) (add (-1) (-2))
 
+let test_add_pos_and_neg () =
+  Alcotest.(check int) "should be -1" (-1) (add 1 (-2))
+
 let test_set = [
   "add positives" ,`Quick, test_add_positives;
-  "add negatives", `Quick, test_add_negatives
+  "add negatives", `Quick, test_add_negatives;
+  "add postive and negative", `Quick, test_add_pos_and_neg;
 ]
 
 let () =
